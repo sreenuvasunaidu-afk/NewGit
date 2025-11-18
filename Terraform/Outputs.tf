c33fede9540f88395dd99ad36f0cdd9a298f9ac0
sreenuvasunaidu-afk <sreenuvasu.naidu@gmail.com>
2025-11-18 20:14:20 +0530

    Add outputs for EC2, VPC, and subnet IDs

--- Terraform/Outputs.tf
+++ Terraform/Outputs.tf
@@ -1,1 +1,12 @@
+output "ec2_public_ip" {
+  description = "Public IP of EC2 instance"
+  value       = aws_instance.web.public_ip
+}
 
+output "vpc_id" {
+  value = aws_vpc.main_vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
